65c60610cd5380006922bfabb2f11943530c2d61
jose.pereda <jose.pereda@gluonhq.com>
2019-10-14 11:40:40 +0200

    Add tests (and replace script.sh)

---
+++ src/test/java/com/gluonhq/substrate/HelloWorldTest.java
@@ -0,0 +1,27 @@
+package com.gluonhq.substrate;
+
+import org.gradle.testkit.runner.BuildResult;
+import org.gradle.testkit.runner.GradleRunner;
+import org.gradle.testkit.runner.TaskOutcome;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class HelloWorldTest {
+
+    @Test
+    void helloWorldTest() {
+        BuildResult result = GradleRunner.create()
+                .withProjectDir(new File("test-project"))
+                .withGradleVersion("5.0")
+                .withArguments("clean", "build", "run", "runScript", "--stacktrace")
+                .forwardOutput()
+                .build();
+
+        assertEquals(TaskOutcome.SUCCESS, result.task(":helloWorld:run").getOutcome(), "Failed build!");
+        assertEquals(TaskOutcome.SUCCESS, result.task(":helloWorld:runScript").getOutcome(), "Failed build!");
+    }
+
+}
---
+++ test-project/helloWorld/src/main/java/com/gluonhq/substrate/test/Main.java
@@ -0,0 +1,10 @@
+package com.gluonhq.substrate.test;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.exit(0);
+    }
+
+}
