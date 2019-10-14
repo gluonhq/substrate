e2e3fde9db8cc1fe1949525c652cc431f1be825a
Johan Vos <johan.vos@gluonhq.com>
2019-10-14 19:53:31 +0200

    Add missing file

---
+++ src/main/resources/native/ios/AppDelegate.m
@@ -0,0 +1,112 @@
+/*
+ * Copyright (c) 2019, Gluon
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL GLUON BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+#import <UIKit/UIKit.h>
+#include <stdarg.h>
+
+static __inline__ void gvmlog(NSString* format, ...)
+{
+    va_list argList;
+    va_start(argList, format);
+    NSString* formattedMessage = [[NSString alloc] initWithFormat: format arguments: argList];
+    va_end(argList);
+    fprintf(stderr, "[GVM] %s\n", [formattedMessage UTF8String]);
+    [formattedMessage release];
+}
+
+#ifdef GVM_VERBOSE
+    #define gvmlog(MSG, ...) gvmlog(MSG, ## __VA_ARGS__ )
+#else
+    #define gvmlog(MSG, ...) (void) 0
+#endif
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
+int startGVM();
+
+extern void *IsolateEnterStub__JavaMainWrapper__run__5087f5482cc9a6abc971913ece43acb471d2631b__a61fe6c26e84dd4037e4629852b5488bfcc16e7e();
+
+@interface AppDelegate ()
+
+@end
+
+@implementation AppDelegate
+
+-(void)startVM:(id)selector {
+    gvmlog(@"Starting vm...");
+    startGVM();
+}
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    gvmlog(@"UIApplication launched!");
+    [self performSelectorInBackground:@selector(startVM:) withObject:NULL];
+    gvmlog(@"UIApplication started GVM in a separate thread");
+    return YES;
+}
+
+
+- (void)applicationWillResignActive:(UIApplication *)application {
+    gvmlog(@"[UIAPP] applicationWillResignActive");
+}
+
+
+- (void)applicationDidEnterBackground:(UIApplication *)application {
+    gvmlog(@"[UIAPP] applicationDidEnterBackground");
+}
+
+
+- (void)applicationWillEnterForeground:(UIApplication *)application {
+    gvmlog(@"[UIAPP] applicationWillEnterForeground");
+}
+
+
+- (void)applicationDidBecomeActive:(UIApplication *)application {
+    gvmlog(@"[UIAPP] applicationDidBecomeActive");
+}
+
+
+- (void)applicationWillTerminate:(UIApplication *)application {
+    gvmlog(@"[UIAPP] applicationWillTerminate");
+}
+
+
+@end
+
+
+int startGVM() {
+    gvmlog(@"Starting GVM for ios");
+
+    (*IsolateEnterStub__JavaMainWrapper__run__5087f5482cc9a6abc971913ece43acb471d2631b__a61fe6c26e84dd4037e4629852b5488bfcc16e7e)(1);
+
+    gvmlog(@"Finished running GVM, done with isolatehread");
+    return 0;
+}
