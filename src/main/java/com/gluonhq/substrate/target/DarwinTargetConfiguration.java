49a56ef2f965a55984b8fcb73cd2d72bccb00bff
jose.pereda <jose.pereda@gluonhq.com>
2019-11-18 12:58:03 +0100

    #95 Add stdc++ library to darwin, iOS and Android

--- src/main/java/com/gluonhq/substrate/target/DarwinTargetConfiguration.java
+++ src/main/java/com/gluonhq/substrate/target/DarwinTargetConfiguration.java
@@ -63,7 +63,7 @@ public class DarwinTargetConfiguration extends AbstractTargetConfiguration {
 
     private static final List<String> macoslibs = Arrays.asList("-lffi",
             "-lpthread", "-lz", "-ldl", "-lstrictmath", "-llibchelper",
-            "-ljava", "-lnio", "-lzip", "-lnet", "-ljvm", "-lobjc",
+            "-ljava", "-lnio", "-lzip", "-lnet", "-ljvm", "-lobjc", "-lstdc++",
             "-Wl,-framework,Foundation", "-Wl,-framework,AppKit",
             "-Wl,-framework,ApplicationServices", "-Wl,-framework,OpenGL",
             "-Wl,-framework,QuartzCore", "-Wl,-framework,Security");
