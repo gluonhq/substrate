e0e3c0774410e5ba9bf0d3b50dfcef3ac843f1cd
José Pereda <pereda@eii.uva.es>
2019-11-22 14:47:03 +0100

    Run tests with GraalVM 19.3 unless skipGraalvmHome is set (#116)

--- src/test/java/com/gluonhq/substrate/IOSTest.java
+++ src/test/java/com/gluonhq/substrate/IOSTest.java
@@ -106,6 +106,7 @@ class IOSTest {
                 .withProjectDir(new File("test-project"))
                 .withGradleVersion("5.3")
                 .withArguments(":helloWorld:clean", ":helloWorld:build",
+                        "-PskipGraalvmHome",
                         "-Dsubstrate.target=ios", "-Dskipsigning=" + skipSigning,
                         ":helloWorld:run", ":helloWorld:runScript", "--stacktrace")
                 .forwardOutput()
@@ -123,6 +124,7 @@ class IOSTest {
                 .withProjectDir(new File("test-project"))
                 .withGradleVersion("5.3")
                 .withArguments(":helloFX:clean", ":helloFX:build",
+                        "-PskipGraalvmHome",
                         "-Dsubstrate.target=ios", ":helloFX:run", ":helloFX:runScript", "--stacktrace")
                 .forwardOutput()
                 .build();
