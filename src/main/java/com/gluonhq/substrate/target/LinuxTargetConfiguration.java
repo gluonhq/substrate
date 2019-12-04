545edc1cf72867da40b11ba739990c42879c0039
jose.pereda <jose.pereda@gluonhq.com>
2019-12-04 20:19:11 +0100

    Add missing import

--- src/main/java/com/gluonhq/substrate/target/LinuxTargetConfiguration.java
+++ src/main/java/com/gluonhq/substrate/target/LinuxTargetConfiguration.java
@@ -37,6 +37,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.LinkedList;
