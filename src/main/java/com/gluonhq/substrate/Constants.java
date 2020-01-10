113c75d8e60cf0869cf7fbaeeb571113bcb16277
Johan Vos <johan@lodgon.com>
2020-01-10 10:05:49 +0100

    add missing profile

--- src/main/java/com/gluonhq/substrate/Constants.java
+++ src/main/java/com/gluonhq/substrate/Constants.java
@@ -99,6 +99,7 @@ public class Constants {
     public static final String PROFILE_IOS = "ios";
     public static final String PROFILE_IOS_SIM = "ios-sim";
     public static final String PROFILE_ANDROID = "android";
+    public static final String PROFILE_LINUX_AARCH64 = "linux-aarch64";
 
     public static final String DEFAULT_JAVA_STATIC_SDK_VERSION  = "14-ea+5";
     public static final String DEFAULT_JAVAFX_STATIC_SDK_VERSION  = "14-ea+gvm4";
