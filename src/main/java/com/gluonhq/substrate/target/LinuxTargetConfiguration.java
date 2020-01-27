d50a3cf9eb8cae88cac5fafc4f26e86b4dc4c653
Lazar Mitrović <lazar.mitrovic996@gmail.com>
2020-01-27 14:24:30 +0100

    Remove unused import

--- src/main/java/com/gluonhq/substrate/target/LinuxTargetConfiguration.java
+++ src/main/java/com/gluonhq/substrate/target/LinuxTargetConfiguration.java
@@ -44,7 +44,6 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.stream.Collectors;
