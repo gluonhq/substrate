1ff5c3870cb9c47e247f605c65e15caa0b949bfb
Johan Vos <johan.vos@gluonhq.com>
2020-02-04 14:45:47 +0100

    check on llc binary

--- src/main/java/com/gluonhq/substrate/target/IosTargetConfiguration.java
+++ src/main/java/com/gluonhq/substrate/target/IosTargetConfiguration.java
@@ -110,16 +110,16 @@ public class IosTargetConfiguration extends PosixTargetConfiguration {
     List<String> getTargetSpecificAOTCompileFlags() throws IOException {
         boolean graalvm22 = true;
         Path internalLlcPath = projectConfiguration.getGraalPath().resolve("lib").resolve("llvm").resolve("bin");
-        if (!Files.exists(internalLlcPath)) {
+        if (!Files.exists(internalLlcPath.resolve("llc"))) {
             graalvm22 = false; // and use customLLC
         }
-        Path llcPath = getLlcPath();
         List<String> answer = Arrays.asList("-H:CompilerBackend=" + Constants.BACKEND_LLVM,
                 "-H:-SpawnIsolates",
                 "-Dllvm.bin.dir=" + internalLlcPath,
                 "-Dsvm.targetName=iOS",
                 "-Dsvm.targetArch=" + getArch());
         if (!graalvm22) {
+            Path llcPath = getLlcPath();
             answer.add("-H:CustomLLC=" + llcPath.toAbsolutePath().toString());
         }
         return answer;
