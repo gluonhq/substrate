d20c7b19eaddba816d77132ed8e2ff21b4c87d73
José Pereda <pereda@eii.uva.es>
2020-02-05 15:18:24 +0100

    #317 Check if app exist before running (#321)
    * #317 Check if app exist before running
    
    * Remove unused import
    
    * Changes based on feedback
    
    * More refactoring

--- src/main/java/com/gluonhq/substrate/target/IosTargetConfiguration.java
+++ src/main/java/com/gluonhq/substrate/target/IosTargetConfiguration.java
@@ -183,14 +183,19 @@ public class IosTargetConfiguration extends PosixTargetConfiguration {
             // without signing, app can't be deployed
             return true;
         }
+
+        Path app = paths.getAppPath().resolve(projectConfiguration.getAppName() + ".app");
+        if (!Files.exists(app)) {
+            throw new IOException("Application not found at path " + app);
+        }
+
         Deploy deploy = new Deploy();
         deploy.addDebugSymbolInfo(paths.getAppPath(), projectConfiguration.getAppName());
-        String appPath = paths.getAppPath().resolve(projectConfiguration.getAppName() + ".app").toString();
         if (isSimulator()) {
             // TODO: launchOnSimulator(appPath);
             return false;
         } else {
-            return deploy.install(appPath);
+            return deploy.install(app.toString());
         }
     }
 
