8ebaba5da6b54cf29b1074baf214fef318a9f41e
jose.pereda <jose.pereda@gluonhq.com>
2020-02-10 20:15:09 +0100

    #323: Add a log file with process command, output and result, if required, and if the process fails #51: Add javadoc

--- src/main/java/com/gluonhq/substrate/util/ProcessRunner.java
+++ src/main/java/com/gluonhq/substrate/util/ProcessRunner.java
@@ -27,11 +27,15 @@
  */
 package com.gluonhq.substrate.util;
 
+import com.gluonhq.substrate.Constants;
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
@@ -40,69 +44,186 @@ import java.util.List;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
+/**
+ * Utility class to run processes based on command line arguments
+ * by wrapping a {@Link ProcessBuilder}.
+ *
+ */
 public class ProcessRunner {
 
     private final List<String> args = new ArrayList<>();
     private final Map<String, String> map;
     private StringBuffer answer;
     private static boolean info;
+    private static boolean logToFile;
+    private final Path processLogPath;
 
+    /**
+     * Constructor, allowing some command line arguments
+     * @param args A varargs of command line arguments
+     */
     public ProcessRunner(String... args) {
         this.args.addAll(Arrays.asList(args));
         this.answer = new StringBuffer();
         this.map = new HashMap<>();
+        // TODO: This is a path for Maven only
+        processLogPath = Path.of(System.getProperty("user.dir"),"target", Constants.CLIENT_PATH, Constants.LOG_PATH);
     }
 
+    /**
+     * When set to true, it will log with Level.INFO the output
+     * during the process. By default is false, and uses Level.DEBUG
+     * @param info a boolean that sets the log level of the process output
+     */
     public void setInfo(boolean info) {
-        this.info = info;
+        ProcessRunner.info = info;
     }
 
+    /**
+     * When set to true, it will log to a file the command line
+     * arguments, the output and the result of the process. By default
+     * it is false, but the log will be created regardless this value
+     * for any failing process.
+     * @param logToFile if true will log the process to a file
+     */
+    public void setLogToFile(boolean logToFile) {
+        ProcessRunner.logToFile = logToFile;
+    }
+    /**
+     * Adds a command line argument to the list of existing list of
+     * command line arguments
+     * @param arg a string passed to the command line arguments
+     */
     public void addArg(String arg) {
         args.add(arg);
     }
 
+    /**
+     * Adds a varargs list of arguments to the existing list of
+     * command line of arguments
+     * @param args varargs list of arguments
+     */
     public void addArgs(String... args) {
         this.args.addAll(Arrays.asList(args));
     }
 
+    /**
+     * Adds a collection of arguments to the existing list of
+     * command line of arguments
+     * @param args a collection of arguments
+     */
     public void addArgs(Collection<String> args) {
         this.args.addAll(args);
     }
 
+    /**
+     *
+     * @return the command line of arguments as a string
+     */
     public String getCmd() {
         return String.join(" ", args);
     }
 
+    /**
+     *
+     * @return the current list of command line arguments
+     */
+    public List<String> getCmdList() {
+        return args;
+    }
+
+    /**
+     * Adds a pair (key, value) to the environment map of the
+     * process
+     * @param key a string with the environmental variable name
+     * @param value a string with the environmental variable value
+     */
     public void addToEnv(String key, String value) {
         map.put(key, value);
     }
 
+    /**
+     * Runs a process with a given set of command line arguments
+     *
+     * @param processName the name of the process
+     * @return 0 if the process ends successfully, non-zero values indicate a failure
+     * @throws IOException
+     * @throws InterruptedException
+     */
     public int runProcess(String processName) throws IOException, InterruptedException {
         return runProcess(processName, null);
     }
 
+    /**
+     * Runs a process with a given set of command line arguments, in a given
+     * working directory
+     *
+     * @param processName the name of the process
+     * @param directory a file with the working directory of the process
+     * @return 0 if the process ends successfully, non-zero values indicate a failure
+     * @throws IOException
+     * @throws InterruptedException
+     */
     public int runProcess(String processName, File directory) throws IOException, InterruptedException {
         Process p = setupProcess(processName, directory);
+
         Thread logThread = mergeProcessOutput(p.getInputStream(), answer);
-        int res = p.waitFor();
+        int result = p.waitFor();
         logThread.join();
-        Logger.logDebug("Result for " + processName + ": " + res);
-        return res;
+        Logger.logDebug("Result for " + processName + ": " + result);
+        if (result != 0) {
+            Logger.logSevere("Process " + processName + " failed with result: " + result);
+        }
+        if (logToFile || result != 0) {
+            logFailure(processName, "result: " + result);
+        }
+        return result;
     }
 
+    /**
+     * Runs a process with a given set of command line arguments within a given time frame
+     *
+     * @param processName the name of the process
+     * @param timeout the maximum time allowed to run the process
+     * @return true if the process ended successfully, false otherwise
+     * @throws IOException
+     * @throws InterruptedException
+     */
     public boolean runTimedProcess(String processName, long timeout) throws IOException, InterruptedException {
         return runTimedProcess(processName, null, timeout);
     }
 
+    /**
+     * Runs a process with a given set of command line arguments, in a given
+     * working directory, within a given time frame
+     *
+     * @param processName the name of the process
+     * @param directory a file with the working directory of the process
+     * @param timeout the maximum time allowed to run the process
+     * @return true if the process ended successfully, false otherwise
+     * @throws IOException
+     * @throws InterruptedException
+     */
     public boolean runTimedProcess(String processName, File directory, long timeout) throws IOException, InterruptedException {
         Process p = setupProcess(processName, directory);
         Thread logThread = mergeProcessOutput(p.getInputStream(), answer);
-        boolean res = p.waitFor(timeout, TimeUnit.SECONDS);
+        boolean result = p.waitFor(timeout, TimeUnit.SECONDS);
         logThread.join();
-        Logger.logDebug("Result for " + processName + ": " + res);
-        return res;
+        Logger.logDebug("Result for " + processName + ": " + result);
+        if (!result) {
+            Logger.logSevere("Process " + processName + " failed with result: " + result);
+        }
+        if (logToFile || !result) {
+            logFailure(processName, "result: " + result);
+        }
+        return result;
     }
 
+    /**
+     * Gets the response of the process as single string
+     *
+     * @return a single string with the whole output of the process
+     */
     public String getResponse() {
         if (answer != null) {
             return answer.toString().replace("\n", "");
@@ -110,11 +231,21 @@ public class ProcessRunner {
         return null;
     }
 
+    /**
+     * Gets the response of the process as list of lines
+     *
+     * @return a list with all the lines of the output
+     */
     public List<String> getResponses() {
         return answer == null ? null :
                 Arrays.asList(answer.toString().split("\n"));
     }
 
+    /**
+     * Gets the last line of the output process
+     *
+     * @return a string with the last line of the output
+     */
     public String getLastResponse() {
         if (answer == null) {
             return null;
@@ -123,6 +254,18 @@ public class ProcessRunner {
         return answers.length > 0 ? answers[answers.length - 1] : "";
     }
 
+    /**
+     * Static method that can be used to process a given command line,
+     * returning the output of the process as a single string, or null if
+     * it failed.
+     * It is convenient when the output of the process to be executed
+     * returns a single line
+     * @param name the name of the process
+     * @param args a varargs list of command line arguments
+     * @return a string with the response of the process or null if it failed
+     * @throws IOException
+     * @throws InterruptedException
+     */
     public static String runProcessForSingleOutput(String name, String... args) throws IOException, InterruptedException {
         ProcessRunner process = new ProcessRunner(args);
         int result = process.runProcess(name);
@@ -166,4 +309,29 @@ public class ProcessRunner {
         return thread;
     }
 
+    /**
+     * Logs to a file the command line arguments, the whole output and the
+     * result of the process.
+     * @param processName The name of the process
+     * @param result The result of the process
+     * @throws IOException
+     */
+    private void logFailure(String processName, String result) throws IOException {
+        if (processLogPath == null) {
+            return;
+        }
+        if (!Files.exists(processLogPath)) {
+            Files.createDirectories(processLogPath);
+        }
+        Path log = processLogPath.resolve("process-" + processName + "-" + System.currentTimeMillis() + ".log");
+        Logger.logInfo("Logging process [" + processName + "] to file: " + log);
+        Files.write(log, toString(processName, result).getBytes());
+    }
+
+    private String toString(String processName, String result) {
+        return "Process\n=======\n" + processName + "\n\n" +
+                "Command Line\n============\n" + getCmd() + "\n\n" +
+                "Output\n======\n" + answer.toString() + "\n\n" +
+                "Result\n======\n" + result;
+    }
 }
