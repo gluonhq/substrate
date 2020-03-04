f75e8bce2fa71f0264ad35ca747d960b815c75cb
jose.pereda <jose.pereda@gluonhq.com>
2020-03-04 10:00:53 +0100

    Update wrapper version to latest 6.2.1 Update tests to same version

--- src/test/java/com/gluonhq/substrate/IOSTest.java
+++ src/test/java/com/gluonhq/substrate/IOSTest.java
@@ -104,7 +104,6 @@ class IOSTest {
 
         BuildResult result = GradleRunner.create()
                 .withProjectDir(new File("test-project"))
-                .withGradleVersion("5.3")
                 .withArguments(":helloWorld:clean", ":helloWorld:build",
                         "-PskipGraalvmHome",
                         "-Dsubstrate.target=ios", "-Dskipsigning=" + skipSigning,
@@ -122,7 +121,6 @@ class IOSTest {
 
         BuildResult result = GradleRunner.create()
                 .withProjectDir(new File("test-project"))
-                .withGradleVersion("5.3")
                 .withArguments(":helloFX:clean", ":helloFX:build",
                         "-PskipGraalvmHome",
                         "-Dsubstrate.target=ios", ":helloFX:run", ":helloFX:runScript", "--stacktrace")
