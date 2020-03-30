359e37352ae9c6d373c743f4c599da4cef6cbe9f
José Pereda <pereda@eii.uva.es>
2020-03-30 11:05:31 +0200

    #477 Use HandlerThread to halt execution while asking for permissions to the user (#479)
    Co-authored-by: jose.pereda <jose.pereda@gluonhq.com>

--- src/main/resources/native/android/dalvik/source/PermissionRequestActivity.java
+++ src/main/resources/native/android/dalvik/source/PermissionRequestActivity.java
@@ -31,7 +31,15 @@ import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.HandlerThread;
+import android.os.Message;
+import android.os.Messenger;
+import android.os.RemoteException;
 import android.util.Log;
+
+import java.util.Arrays;
 
 /**
  * If targetSdkVersion &gt;= 23, and any of the plugin requires a dangerous
@@ -50,47 +58,70 @@ public class PermissionRequestActivity extends Activity {
     private static final String TAG     = "GraalActivity";
 
     private static final String KEY_PERMISSIONS = "permissions";
+    private static final String KEY_MESSENGER = "messenger";
     private static final String KEY_REQUEST_CODE = "requestCode";
     private static final int PERMISSION_REQUEST_CODE = 10010;
 
-    private static String[] permissions;
+    private static boolean result;
+    private static Messenger messenger;
     private static int requestCode;
 
     @Override
     protected void onStart() {
         super.onStart();
         Log.v(TAG, "PermissionRequestActivity::onStart");
-        permissions = this.getIntent().getStringArrayExtra(KEY_PERMISSIONS);
+        String[] permissions = this.getIntent().getStringArrayExtra(KEY_PERMISSIONS);
         requestCode = this.getIntent().getIntExtra(KEY_REQUEST_CODE, 0);
-
+        messenger = this.getIntent().getParcelableExtra(KEY_MESSENGER);
         if (permissions == null) {
-            Log.e(TAG, "PermissionRequestActivity: no permissions found");
+            Log.e(TAG, "PermissionRequestActivity: no permissions were found");
             finish();
             return;
         }
 
-        Log.v(TAG, "PermissionRequestActivity::requesting permissions");
+        if (verify(this, permissions)) {
+            int[] result = new int[permissions.length];
+            Arrays.fill(result, PackageManager.PERMISSION_GRANTED);
+            Log.v(TAG, "PermissionRequestActivity: All permissions are granted");
+            processResult(result);
+        }
+
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            Log.v(TAG, "PermissionRequestActivity::requesting permissions");
             requestPermissions(permissions, requestCode);
         } else {
-            Log.v(TAG, "requestPermissions requires Build.VERSION.SDK_INT (" + Build.VERSION.SDK_INT+ ") >= 23");
+            Log.v(TAG, "RequestPermissions requires Build.VERSION.SDK_INT (" + Build.VERSION.SDK_INT+ ") >= 23");
         }
     }
 
     @Override
-    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
         if (PermissionRequestActivity.requestCode == requestCode) {
             Log.v(TAG, "PermissionRequestActivity::onRequestPermissionsResult");
-            verify(this, permissions);
-            finish();
+            processResult(grantResults);
         }
     }
 
-    private static void requestPermission(Activity activity, String[] permissionsName) {
+    private void processResult(int[] grantResults) {
+        try {
+            Message message = Message.obtain();
+            Bundle bundle = new Bundle();
+            bundle.putIntArray(KEY_PERMISSIONS, grantResults);
+            message.setData(bundle);
+            messenger.send(message);
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
+        finish();
+    }
+
+    private static void requestPermission(Activity activity, String[] permissionsName, Messenger messenger) {
         Intent intent = new Intent(activity, PermissionRequestActivity.class);
         intent.putExtra(KEY_PERMISSIONS, permissionsName);
         intent.putExtra(KEY_REQUEST_CODE, PERMISSION_REQUEST_CODE);
+        intent.putExtra(KEY_MESSENGER, messenger);
         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+
         Log.v(TAG, "PermissionRequestActivity::start intent to requestPermission");
         activity.startActivity(intent);
     }
@@ -102,21 +133,47 @@ public class PermissionRequestActivity extends Activity {
         for (String permission : permissionsName) {
             int result = activity.checkSelfPermission(permission);
             if (result != PackageManager.PERMISSION_GRANTED) {
-                Log.v(TAG, String.format("Permission %s not granted", permission));
+                Log.v(TAG, String.format("Permission %s is not granted", permission));
                 return false;
             }
         }
-        Log.v(TAG, String.format("All requested permissions are granted"));
+        Log.v(TAG, "All requested permissions are granted");
         return true;
     }
 
     public static boolean verifyPermissions(final Activity activity, final String[] permissionsName) {
-        Log.v(TAG, String.format("PermissionRequestActivity::Calling verifyPermissions"));
+        Log.v(TAG, "PermissionRequestActivity::Calling verifyPermissions");
         if (verify(activity, permissionsName)) {
             return true;
         }
-        requestPermission(activity, permissionsName);
-        return false;
+        final HandlerThread handlerThread = new HandlerThread("handler thread");
+        handlerThread.start();
+
+        Handler handler = new Handler(handlerThread.getLooper()) {
+            @Override
+            public void handleMessage(Message msg) {
+                Log.v(TAG, "Received message in handler: " + msg);
+                int[] results = msg.getData().getIntArray(KEY_PERMISSIONS);
+                result = true;
+                for (int i = 0; i < results.length; i++) {
+                    if (results[i] != PackageManager.PERMISSION_GRANTED) {
+                        result = false;
+                        break;
+                    }
+                }
+                handlerThread.quit();
+            }
+        };
+
+        Messenger messenger = new Messenger(handler);
+        requestPermission(activity, permissionsName, messenger);
+
+        try {
+            handlerThread.join();
+        } catch (InterruptedException e) {
+            return false;
+        }
+        return result;
     }
 
 }
