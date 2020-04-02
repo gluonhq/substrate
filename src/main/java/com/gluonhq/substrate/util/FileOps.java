3a1b0114fa2b23398cf0d1b3a8f190c0a9316c12
Abhinay Agarwal <abhinay.agarwal@gluonhq.com>
2020-04-02 15:40:54 +0530

    Change to string as per feedback

--- src/main/java/com/gluonhq/substrate/util/FileOps.java
+++ src/main/java/com/gluonhq/substrate/util/FileOps.java
@@ -543,7 +543,7 @@ public class FileOps {
         }
 
         public void rbcProgressCallback(RBCWrapper rbc) {
-            System.out.print("\r" + String.format("Download Progress: %.2f / %.2fM", toMB(rbc.readSoFar), toMB(rbc.expectedSize)));
+            System.out.print("\r" + String.format("Download progress: %.2f / %.2fM", toMB(rbc.readSoFar), toMB(rbc.expectedSize)));
             System.out.flush();
         }
 
