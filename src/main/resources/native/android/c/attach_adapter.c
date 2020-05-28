5c24e07f21ac18def2df51093887ceba1da154d5
jose.pereda <jose.pereda@gluonhq.com>
2020-05-28 19:53:46 +0200

    Add support for Share service

--- src/main/resources/native/android/c/attach_adapter.c
+++ src/main/resources/native/android/c/attach_adapter.c
@@ -38,6 +38,7 @@ jclass jPicturesServiceClass;
 jclass jPositionServiceClass;
 jclass jOrientationServiceClass;
 jclass jSettingsServiceClass;
+jclass jShareServiceClass;
 jclass jStorageServiceClass;
 int handlesInitialized = 0;
 
@@ -68,6 +69,7 @@ void registerAttachMethodHandles(JNIEnv* androidEnv) {
     jPositionServiceClass = registerClass(androidEnv, "com/gluonhq/helloandroid/DalvikPositionService");
     jOrientationServiceClass = registerClass(androidEnv, "com/gluonhq/helloandroid/DalvikOrientationService");
     jSettingsServiceClass = registerClass(androidEnv, "com/gluonhq/helloandroid/DalvikSettingsService");
+    jShareServiceClass = registerClass(androidEnv, "com/gluonhq/helloandroid/DalvikShareService");
     jStorageServiceClass = registerClass(androidEnv, "com/gluonhq/helloandroid/DalvikStorageService");
     handlesInitialized = 1;
 }
@@ -114,6 +116,10 @@ jclass substrateGetOrientationServiceClass() {
 
 jclass substrateGetSettingsServiceClass() {
     return jSettingsServiceClass;
+}
+
+jclass substrateGetShareServiceClass() {
+    return jShareServiceClass;
 }
 
 jclass substrateGetStorageServiceClass() {
