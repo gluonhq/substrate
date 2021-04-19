42e774ee7acfee53198739441f66dbe0a6d512f0
jose.pereda <jose.pereda@gluonhq.com>
2021-04-19 14:19:56 +0200

    Bump JavaFX static version

--- src/main/java/com/gluonhq/substrate/Constants.java
+++ src/main/java/com/gluonhq/substrate/Constants.java
@@ -103,7 +103,7 @@ public class Constants {
 
     // public static final String DEFAULT_JAVA_STATIC_SDK_VERSION  = "15-ea+4";
     public static final String DEFAULT_JAVA_STATIC_SDK_VERSION  = "11-ea+1";
-    public static final String DEFAULT_JAVAFX_STATIC_SDK_VERSION  = "17-ea+gvm1";
+    public static final String DEFAULT_JAVAFX_STATIC_SDK_VERSION  = "17-ea+7";
 
     /**
      * Supported GraalVM versions
