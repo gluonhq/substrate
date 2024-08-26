d7abf41a62c5c119992ea29ac0559582a0acc99f
jose.pereda <jose.pereda@gluonhq.com>
2024-08-26 23:34:11 +0200

    fix font issue on Android

--- src/main/resources/native/android/c/launcher.c
+++ src/main/resources/native/android/c/launcher.c
@@ -69,6 +69,7 @@ const char *origArgs[] = {
     "-Dmonocle.input.touchRadius=1",
     "-Dmonocle.input.traceEvents.verbose=false",
     "-Dprism.verbose=true",
+    "-Dprism.useFontConfig=true",
     "-Xmx4g"};
 
 void registerMethodHandles(JNIEnv *aenv)
