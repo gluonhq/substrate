ff52cb4118a02bb7b03ed1ece58aa686805a9bc0
Devin Gillman <devin.gillman@gmail.com>
2025-04-20 10:57:29 -0500

    Updating the copyright header.

--- src/main/java/com/gluonhq/substrate/target/PosixTargetConfiguration.java
+++ src/main/java/com/gluonhq/substrate/target/PosixTargetConfiguration.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019, 2022, Gluon
+ * Copyright (c) 2019, 2022, 2025, Gluon
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
